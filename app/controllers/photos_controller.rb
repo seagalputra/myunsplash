d61590e5d9ad3352a2cc74de9c263ef7677104c9
seagalputra <seagalputra@gmail.com>
2021-05-15 13:33:56 +0700

    Refactor into model callback

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,14 +5,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    photo_data = photo_params
-    photo_size = FastImage.size(photo_data['url'])
-
-    photo_data['width'] = photo_size[0]
-    photo_data['height'] = photo_size[1]
-    photo_data['password'] = ENV['APP_PASSWORD']
-
-    @photo = Photo.new(photo_data)
+    @photo = Photo.new(photo_params)
 
     redirect_to action: 'index' if @photo.save
   end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,19 @@
 class Photo < ApplicationRecord
   has_secure_password
   validates :password, presence: true
+  
+  before_validation :store_user_password
+  before_save :store_image_size
+
+  private
+
+  def store_user_password
+    self.password = ENV['APP_PASSWORD']
+  end
+
+  def store_image_size
+    photo_size = FastImage.size(url)
+    self.width = photo_size[0]
+    self.height = photo_size[1]
+  end
 end
